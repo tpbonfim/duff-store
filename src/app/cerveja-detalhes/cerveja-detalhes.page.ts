0b7068362cb686987647ac09f38a0db4f2e17ac6
TATIANE PEREIRA BONFIM <509660204@alunos.etec.itaqua>
2019-03-28 20:41:10 -0300

    Foi feita a criaçaõ de uma nova pagina, um botaõ de navegaçaõ e a implementação do construtor

---
+++ src/app/cerveja-detalhes/cerveja-detalhes.page.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cerveja-detalhes',
+  templateUrl: './cerveja-detalhes.page.html',
+  styleUrls: ['./cerveja-detalhes.page.scss'],
+})
+export class CervejaDetalhesPage implements OnInit {
+
+  nome;
+  detalhes;
+
+  constructor() { 
+    this.nome="Puro Malte";
+    this.detalhes="Tão gostosa que é proibida. A melhor puro malte do Ceara";
+    
+  }
+
+  ngOnInit() {
+  }
+
+}
